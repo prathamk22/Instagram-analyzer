6a790bf3ac14a3473ad42a0c52d86b17135820d3
prathamk22 <prathamkhurana43@gmail.com>
2021-01-31 18:56:26 +0530

    Hilt Dependencies setup done.

--- app/src/main/java/com/pratham/project/fileio/InstaAnalyzerApp.kt
+++ app/src/main/java/com/pratham/project/fileio/InstaAnalyzerApp.kt
@@ -5,9 +5,11 @@ import com.pratham.project.fileio.di.databaseModule
 import com.pratham.project.fileio.di.instagramApiModule
 import com.pratham.project.fileio.di.preferencesModule
 import com.pratham.project.fileio.di.viewModelModule
+import dagger.hilt.android.HiltAndroidApp
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
+@HiltAndroidApp
 class InstaAnalyzerApp : Application() {
 
     override fun onCreate() {
